31b98939dc77a7a66ce89590c2e8267ce9a8a1c7
Daniel Csillag <dccsillag@gmail.com>
2021-10-16 09:05:44 -0300

    Fix argument parsing of commands to not try to parse flags, and work around argument parsing of subcommands to not trigger improperly

--- src/main.rs
+++ src/main.rs
@@ -12,57 +12,52 @@ fn main() -> Result<()> {
     let matches = App::new(crate_name!())
         .about("A tool to manage running jobs.")
         .version(crate_version!())
-        .setting(AppSettings::SubcommandsNegateReqs)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::AllowExternalSubcommands)
         .subcommand_placeholder("ACTION", "ACTIONS")
-        .arg(
-            Arg::new("command")
-                .required(true)
-                .multiple_values(true)
-                .about("The command to run"),
-        )
         .subcommand(
-            App::new("list")
+            App::new("-list")
                 .short_flag('l')
                 .long_flag("list")
                 .about("List runs"),
         )
         .subcommand(
-            App::new("info")
+            App::new("-info")
                 .short_flag('i')
                 .long_flag("info")
                 .about("Show information about a run")
                 .arg(Arg::new("run").about("Which run to show information on")),
         )
         .subcommand(
-            App::new("view")
+            App::new("-view")
                 .short_flag('v')
                 .long_flag("view")
                 .about("View a run")
                 .arg(Arg::new("run").about("Which run to view")),
         )
         .subcommand(
-            App::new("remove")
+            App::new("-remove")
                 .short_flag('r')
                 .long_flag("remove")
                 .about("Remove a run")
                 .arg(Arg::new("run").about("Which run to remove")),
         )
         .subcommand(
-            App::new("interrupt")
+            App::new("-interrupt")
                 .short_flag('c')
                 .long_flag("interrupt")
                 .about("Interrupt (SIGINT, Ctrl+C) a run")
                 .arg(Arg::new("run").about("Which run to interrupt")),
         )
         .subcommand(
-            App::new("terminate")
+            App::new("-terminate")
                 .short_flag('t')
                 .long_flag("terminate")
                 .about("Terminate (SIGTERM) a run")
                 .arg(Arg::new("run").about("Which run to terminate")),
         )
         .subcommand(
-            App::new("kill")
+            App::new("-kill")
                 .short_flag('K')
                 .long_flag("kill9")
                 .about("Kill (SIGKILL, kill -9) a run")
@@ -73,33 +68,38 @@ fn main() -> Result<()> {
     let runs = Runs::new().with_context(|| "Could not acquire runs")?;
 
     match matches.subcommand() {
-        None => actions::start::start_run(
-            &runs,
-            matches.values_of_t_or_exit("command"),
-            /*TODO label*/ None,
-        ),
-        Some(("list", _)) => actions::list::list_runs(&runs),
-        Some(("info", submatches)) => {
+        Some(("-list", _)) => actions::list::list_runs(&runs),
+        Some(("-info", submatches)) => {
             actions::show_info::show_run_info(&runs.get_run(&submatches.value_of_t_or_exit("run"))?)
         }
-        Some(("view", submatches)) => {
+        Some(("-view", submatches)) => {
             actions::open::open_run(&runs.get_run(&submatches.value_of_t_or_exit("run"))?)
         }
-        Some(("remove", submatches)) => {
+        Some(("-remove", submatches)) => {
             actions::remove::remove_run(&runs, runs.get_run(&submatches.value_of_t_or_exit("run"))?)
         }
-        Some(("interrupt", submatches)) => actions::send_signal::send_signal(
+        Some(("-interrupt", submatches)) => actions::send_signal::send_signal(
             &runs.get_run(&submatches.value_of_t_or_exit("run"))?,
             signal::Signal::SIGINT,
         ),
-        Some(("terminate", submatches)) => actions::send_signal::send_signal(
+        Some(("-terminate", submatches)) => actions::send_signal::send_signal(
             &runs.get_run(&submatches.value_of_t_or_exit("run"))?,
             signal::Signal::SIGTERM,
         ),
-        Some(("kill", submatches)) => actions::send_signal::send_signal(
+        Some(("-kill", submatches)) => actions::send_signal::send_signal(
             &runs.get_run(&submatches.value_of_t_or_exit("run"))?,
             signal::Signal::SIGKILL,
         ),
+        Some((argv0, submatches)) => {
+            let command = match submatches.values_of("") {
+                Some(s) => std::iter::once(argv0)
+                    .chain(s)
+                    .map(|s| s.to_string())
+                    .collect(),
+                None => std::iter::once(argv0).map(|s| s.to_string()).collect(),
+            };
+            actions::start::start_run(&runs, command, /*TODO label*/ None)
+        }
         _ => unreachable!(),
     }
 }
