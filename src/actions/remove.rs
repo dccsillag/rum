fe1dfe8bd9152351c243c4757e591549913481c7
Daniel Csillag <dccsillag@gmail.com>
2021-10-16 10:49:20 -0300

    Change remove action to reuse info action

--- src/actions/remove.rs
+++ src/actions/remove.rs
@@ -1,24 +1,17 @@
 use anyhow::{Error, Result};
 
-use crate::runs::{Run, RunData, RunDataState, Runs};
+use crate::{
+    actions::show_info::show_run_info,
+    runs::{Run, RunData, RunDataState, Runs},
+};
 
 pub fn remove_run(runs: &Runs, run: Run) -> Result<()> {
     match run.get_data()? {
         RunData {
-            label,
-            command,
-            start_datetime,
-            state:
-                RunDataState::Done {
-                    end_datetime,
-                    exit_code,
-                },
+            state: RunDataState::Done { .. },
+            ..
         } => {
-            label.map(|l| println!("Label: {}", l));
-            println!("Command: {}", shell_words::join(command));
-            println!("Started running: {}", start_datetime);
-            println!("Finished running: {}", end_datetime);
-            println!("Exit code: {}", exit_code);
+            show_run_info(&run)?;
 
             if dialoguer::Confirm::with_theme(&dialoguer::theme::ColorfulTheme::default())
                 .with_prompt("Are you sure you want to delete this run?")
