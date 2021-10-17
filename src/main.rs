374ca2f56e5e63f5863977ca812a036f2c25940e
Daniel Csillag <dccsillag@gmail.com>
2021-10-17 10:37:03 -0300

    Clean up argparse

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@ pub mod runs;
 pub mod utils;
 
 use anyhow::{Context, Result};
-use clap::{crate_name, crate_version, Clap};
+use clap::{crate_name, crate_version, Clap, AppSettings};
 use nix::sys::signal;
 
 use runs::Runs;
 
 #[derive(Clap)]
-#[clap(about = "A tool to manage running jobs.", name = crate_name!(), version = crate_version!())]
+#[clap(
+    about = "A tool to manage running jobs.",
+    name = crate_name!(),
+    version = crate_version!(),
+    override_usage = "rum <COMMAND> [<ARG> [<ARG> [...]]]\n    rum <SUBCOMMAND>",
+)]
+#[clap(setting = AppSettings::DisableHelpSubcommand)]
 struct Args {
     #[clap(subcommand)]
     subcommand: Subcommand,
